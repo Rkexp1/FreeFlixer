05e43fcd1e4e07929d598d4712434b11ee436f11
Rkexp1 <rkexp1@gmail.com>
2021-05-01 23:32:03 +0530

    Exp-1 Done. Can access all jSon data and develop htmlpage  with imgurls working .

--- ver1/scriptdl.js
+++ ver1/scriptdl.js
@@ -5,31 +5,60 @@ const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 const APIS_URL = BASE_URL + '/discover/tv?sort_by=popularity.desc&'+API_KEY;
 
-
-
+let mvObj = {} ;
 
 const mvurl = localStorage.getItem("locmvurl");
 getmvinfo(mvurl);
-
 function getmvinfo(url) 
-    {
-        console.log(url);
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {console.log(data)
-        console.log(typeof(data)); //object fixx
-        showmvinfo(data);
-    })
+{
+console.log(url);
+//tryOne
+fetch(url)
+.then(function(response){
+return response.json(); 
+})
+.then(function(data){
+  console.log(data);
+  // Setting-up  varibles
+  mvname = data.title;
+  mvtime=data.runtime;
+  mvObj = data;
+  showmvinfo();
+});
+//tryOne ends
 }
 
 function showmvinfo(data){
+    for (let prop in mainObj){
+        
+        console.log(mainObj[prop]);
+    }
+
     main.innerHTML="";
     const movieEl = document.createElement('div');
     movieEl.classList.add('inmain');
-    document.getElementById("inmain").innerHTML = 
-    movieEl.innerHTML = ` <p> ${data.title} </p>
+    movieEl.innerHTML = ` <p>${mvObj.title}</p>
+    <img src="${IMG_URL+mvObj.poster_path}" alt="${mvObj.title}">
     
     
     `
         main.appendChild(movieEl);
   }
+
+
+
+
+
+
+  // Original
+ /* 
+ fetch(url)
+  .then(response => response.json())
+  .then(data => {console.log(data)
+  console.log(typeof(data)); //object fixx
+  thename=data.title;
+  thetime=data.runtime;
+  showmvinfo(data);                              
+                          })
+*/
+  //
